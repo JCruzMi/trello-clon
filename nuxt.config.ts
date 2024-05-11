3cec0202381b1333d333cb85b198f49c4803a1d3
Huan <juan.murato.cruz@gmail.com>
2024-05-11 08:04:33 -0500

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/trello-clon/",
+    buildAssetsDir: "assets/",
+  },
   ssr: false,
   css: ["~/assets/css/vello.css"],
   devtools: { enabled: false },
